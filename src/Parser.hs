f7ef079c08dfb1d36e5110c53803cfac3047a7e1
Jonathan Chan <afnl686@gmail.com>
2018-10-31 14:53:34 -0700

    Added data type for numbers.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,8 +1,29 @@
+{-# LANGUAGE GADTs #-}
+
 module Parser
     ( parser
     ) where
 
 import qualified Text.Parsec as Parsec
+
+{-
+    EBNF:
+    <FWAE> :: <num>
+            | {<op> <FWAE> <FWAE>}
+            | {with {<id> <FWAE>} <FWAE>}
+            | <id>
+            | {fun {<id>} <FWAE>}
+            | {<FWAE> <FWAE>}
+            | {if0 <FWAE> <FWAE> <FWAE>}
+    <op>   :: + | - | *
+    <id>   :: (* begins with letter, is alphanumeric or `-` or `_` or `*` *)
+              (* is none of `with`, `fun`, `+`, `-`, `*` *)
+-}
+
+data FAE where
+    Number :: (Num a, Show a) => {
+        num :: a
+    } -> FAE
 
 parser :: IO ()
 parser = putStrLn "TODO"
